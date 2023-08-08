aeafcc699e62f3295d6064b8569645dc6c86bdd9
gacetta <78240758+gacetta@users.noreply.github.com>
2023-08-08 10:53:13 -0700

    in-process: section 15

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 const Task = require("../models/task");
 
-const connectionURL = "mongodb://127.0.0.1:27017";
+const connectionURL = "mongodb://127.0.0.1:27017"`;
 const dbName = "task-manager-api";
 mongoose.connect(connectionURL, { dbName });
 
